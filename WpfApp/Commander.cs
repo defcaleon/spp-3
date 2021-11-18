742214ab8c8dc71bd4ca5ac819dfc1748fb2cff9
Nikita Parmon <nikitka.parmon@mail.ru>
2021-11-19 00:31:45 +0300

    wpf app implementation

---
+++ WpfApp/Commander.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class Commander : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public Commander(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ WpfApp/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WpfApp.Annotations;
+using Microsoft.Win32;
+using Browser;
+
+namespace WpfApp
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Commander openSelectAssemblyDialog;
+        public List<Node> Data { get; private set; }
+
+        public Commander OpenSelectAssemblyDialog
+        {
+            get
+            {
+                return openSelectAssemblyDialog ??= new Commander(obj =>
+                {
+                    var openFileDialog = new OpenFileDialog();
+
+                    if (openFileDialog.ShowDialog() ?? false)
+                    {
+                        Data = new List<Node>();
+                        Data.Add(AssemblyBrowser.Browse(openFileDialog.FileName));
+                        OnPropertyChanged(nameof(Data));
+                    }
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+            Data = new();
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
